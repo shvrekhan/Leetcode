380283b3a74c2fbdd41059041e6892d0ebda675b
Shiva Rekhan <52254431+shvrekhan@users.noreply.github.com>
2023-12-24 00:01:55 +0530

    Time: 5 ms (42.95%), Space: 42.4 MB (5.29%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,21 +1,27 @@
-public class Solution {
-    public boolean isPathCrossing(String movementPath) {
+class Solution {
+
+    public boolean isPathCrossing(String path) {
         Set<String> visitedPoints = new HashSet<>();
-        int currentX = 0, currentY = 0;
-        visitedPoints.add(currentX + "," + currentY);
+        int x = 0;
+        int y = 0;
+        visitedPoints.add(x + "," + y);
 
-        for (char direction : movementPath.toCharArray()) {
-            if (direction == 'N') currentY++;
-            else if (direction == 'S') currentY--;
-            else if (direction == 'E') currentX++;
-            else currentX--;
-
-            String point = currentX + "," + currentY;
+        for (char currentDirection : path.toCharArray()) {
+            if (currentDirection == 'N') {
+                y += 1;
+            } else if (currentDirection == 'S') {
+                y -= 1;
+            } else if (currentDirection == 'E') {
+                x += 1;
+            } else {
+                x -= 1;
+            }
+            String point = x + "," + y;
             if (visitedPoints.contains(point)) {
                 return true;
+            } else {
+                visitedPoints.add(point);
             }
-
-            visitedPoints.add(point);
         }
 
         return false;
